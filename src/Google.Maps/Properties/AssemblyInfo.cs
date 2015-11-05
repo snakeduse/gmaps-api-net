1fe5a117ed7f477c8a73d56e780b746cad5ecd02
Aleksey Profatilov <snakeduse@gmail.com>
2015-11-05 14:11:18 +0300

    change version

--- src/Google.Maps/Properties/AssemblyInfo.cs
+++ src/Google.Maps/Properties/AssemblyInfo.cs
@@ -2,25 +2,24 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Google Maps API for .NET Service")]
-[assembly: AssemblyDescription("Google Maps API for .NET service library")]
+[assembly: AssemblyTitle("ZT Google Maps API for .NET Service")]
+[assembly: AssemblyDescription("Google Maps API for .NET service library for Zone Of Teleportation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Google Maps API for .NET")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Zone of Teleportation")]
+[assembly: AssemblyProduct("ZT Google Maps API for .NET")]
+[assembly: AssemblyCopyright("2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("add2eb45-f508-4a72-9d59-fdbc553903af")]
-
-[assembly: AssemblyVersion("0.12.0.36637")]
-[assembly: AssemblyFileVersion("0.12.0.36637")]
+[assembly: AssemblyVersion("0.12.0.36640")]
+[assembly: AssemblyFileVersion("0.12.0.36640")]
